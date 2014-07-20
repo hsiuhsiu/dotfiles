e84a848dbdfe17ab4c254665079fcba1b88ed9fa
Yi-Hsiu Chen <yhchen@rocketfuel.com>
2014-07-20 11:01:00 -0700

    Add git related configs

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ echo $DIR
 echo "append source command in .bashrc"
 echo -e "\nsource ${DIR}/bashrc/main.sh" >> ~/.bashrc
 
-rm -f ~/.bash_profile
 echo "create .bash_profile"
+rm -f ~/.bash_profile
 echo -e "source ~/.bashrc" > ~/.bash_profile
+
+echo "create .gitconfig (symbolic link)"
+ln -s ~/.gitconfig ${DIR}/git/gitconfig
+echo "create .gitignore_global (symbolic link)"
+ln -s ~/.gitignore_global ${DIR}/git/gitignore_global
