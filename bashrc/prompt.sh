ce27adc380125cbc2180e7b920e1d9eeaa517eaa
Yi-Hsiu Chen <hsiuhsiu@gmail.com>
2019-03-22 17:36:07 -0400

    Bug fix: git info does not update in PS1

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -1,7 +1,13 @@
+function git_dirty {
+  [[ -z $(git status --porcelain 2> /dev/null) ]] || echo "*"
+}
+
+function ps1_git {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1)$(git_dirty)/"
+}
+
 function prompt {
   local PROMPT_SYMBOL="\$"
-  GIT_BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
-  GIT_DIRTY=$([[ -z $(git status --porcelain) ]] || echo "*")
 
   local NO_COLOR="\[\033[0m\]"
   local GIT_COLOR="\[\033[1;35m\]"
@@ -15,8 +21,8 @@ function prompt {
     local PROMPT_COLOR="\[\033[0;37m\]"
   fi
 
-  PS1="$TIME_COLOR[\A] $USER_COLOR\u$HOST_COLOR@\h: $PATH_COLOR\w"
-  PS1+="$GIT_COLOR$GIT_BRANCH$GIT_DIRTY\n$PROMPT_COLOR$PROMPT_SYMBOL $NO_COLOR"
+  PS1="$TIME_COLOR[\A] $USER_COLOR\u$HOST_COLOR@\h: $PATH_COLOR\w "
+  PS1+="$GIT_COLOR\$(ps1_git)\n$PROMPT_COLOR$PROMPT_SYMBOL $NO_COLOR"
   PS2='continue-> '
   PS4='$0.$LINENO+ '
 }
