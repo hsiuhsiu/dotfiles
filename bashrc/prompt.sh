ab7136eab88c7348395e6ab2824985fc8ed33373
Yi-Hsiu Chen <yihsiu@fb.com>
2019-08-02 09:10:02 -0700

    Change prompt color on Mac

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -13,6 +13,7 @@ function prompt {
   local GIT_COLOR="\[\033[1;35m\]"
   local TIME_COLOR="\[\033[1;34m\]"
   local USER_COLOR="\[\033[1;34m\]"
+  local MAC_USER_COLOR="\[\033[2;33m\]"
   local HOST_COLOR="\[\033[1;34m\]"
   local PATH_COLOR="\[\033[0;32m\]"
   if [[ ${EUID} == 0 ]] ; then
@@ -21,7 +22,12 @@ function prompt {
     local PROMPT_COLOR="\[\033[0;37m\]"
   fi
 
-  PS1="$TIME_COLOR[\A] $USER_COLOR\u$HOST_COLOR@\h: $PATH_COLOR\w "
+  if [ `uname` = "Linux" ]; then
+    PS1="$TIME_COLOR[\A] $USER_COLOR\u$HOST_COLOR@\h: $PATH_COLOR\w "
+  elif [ `uname` = "Darwin" ]; then # For Mac OSX
+    PS1="$MAC_USER_COLOR[\A] $MAC_USER_COLOR\u: $PATH_COLOR\w "
+  fi
+
   PS1+="$GIT_COLOR\$(ps1_git)\n$PROMPT_COLOR$PROMPT_SYMBOL $NO_COLOR"
   PS2='continue-> '
   PS4='$0.$LINENO+ '
